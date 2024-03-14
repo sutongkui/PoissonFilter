2aa9a6e864f89eaaf80b0fcabb78c62ff31f76c4
tongkuisu <tongkuisu@gmail.com>
2024-03-14 11:13:23 +0800

    Support forward poisson problem in 2D

--- SVD2D.py
+++ SVD2D.py
@@ -1,34 +1,41 @@
+# Ref:
+# Compact Poisson Filters for Fast Fluid Simulation: https://dl.acm.org/doi/pdf/10.1145/3528233.3530737
+# supplementary material: https://github.com/ubisoft/ubisoft-laforge-Poisson-Filters/blob/main/docs/paper/supplementary.pdf
+
 import numpy as np
 import os
 
-# Jacobi iteration steps
-# Adjust this variable to get what you want
-IterationCount = 3
-
 CacheDict = {}
 
-def DivergenceIter(I, J, K):
+class Paras:
+    def __init__(self, size, alpha, belta):
+        self.KernelSize = size
+        self.alpha = alpha
+        self.belta = belta
+
+def ConstructKernel(I, J, K, KernelParas):
     CachedValue = CacheDict.get((I, J, K))
     if CachedValue is not None:
         return CachedValue
-    KernelSize = 2 * IterationCount - 1
-    Kernel = np.zeros((KernelSize, KernelSize))
+
+    Kernel = np.zeros((KernelParas.KernelSize, KernelParas.KernelSize))
     if K == 0:
         return Kernel
     else:
-        XIMinus1J = DivergenceIter(I - 1, J, K - 1)
-        XIPlus1J = DivergenceIter(I + 1, J, K - 1)
-        XIJMinus1 = DivergenceIter(I, J - 1, K - 1)
-        XIJPlus1 = DivergenceIter(I, J + 1, K - 1)
+        X_IMinus1_J = ConstructKernel(I - 1, J, K - 1, KernelParas)
+        X_IPlus1_J = ConstructKernel(I + 1, J, K - 1, KernelParas)
+        X_I_JMinus1 = ConstructKernel(I, J - 1, K - 1, KernelParas)
+        X_I_JPlus1 = ConstructKernel(I, J + 1, K - 1, KernelParas)
         Kernel[I][J] = 1
-        Ret = XIMinus1J * 0.25 + XIPlus1J * 0.25 + XIJMinus1 * 0.25 + XIJPlus1 * 0.25 - Kernel * 0.25
+        Ret = (X_IMinus1_J + X_IPlus1_J + X_I_JMinus1 + X_I_JPlus1 + KernelParas.alpha * Kernel) / KernelParas.belta
         CacheDict[(I, J, K)] = Ret
         return Ret
 		
-def DivergenceKenel(IterationCount):
-    KernelSize = 2 * IterationCount - 1
+def JacobiKernel(Ite, alpha, belta):
+    KernelSize = 2 * Ite - 1
     CenterIndex = KernelSize // 2
-    Kernel = DivergenceIter(CenterIndex, CenterIndex, IterationCount)
+    KernelParas = Paras(KernelSize, alpha, belta)
+    Kernel = ConstructKernel(CenterIndex, CenterIndex, Ite, KernelParas)
     return Kernel
 	
 def Svd(Kernel):
@@ -40,10 +47,51 @@ def ComputeError(matrix1, matrix2):
     error = np.sqrt(np.sum(np.square(difference)))
     return error
 
+# Forward problem, diffusion in fluid simulation
+def ComputeForwardParas():
+
+    viscosity = 10.0
+
+    # 30 frames per second, delta_t = 1.0/30
+    delta_t = 0.033
+
+    # Fix delta_x = 1 m
+    delta_x = 1.0
+    
+    # 2D
+    dimension = 2
+
+    alpha = delta_x**2 / (viscosity * delta_t)
+    belta = 2 * dimension + alpha
+
+    return alpha, belta
+
+
+# Inverse problem, pressure solver in in fluid simulation
+def ComputeInverseParas():
+
+    # Fix delta_x = 1 m
+    delta_x = 1.0
+
+    # 2D
+    dimension = 2
+
+    alpha = - delta_x**2
+    belta = 2.0 * dimension
+
+    return alpha, belta
+ 
 
 if __name__=="__main__":
 
-    Kernel = DivergenceKenel(IterationCount)
+    # Jacobi iteration steps
+    # Adjust this variable to get what you want
+    IterationCount = 2
+
+    #alpha, belta = ComputeInverseParas()
+    alpha, belta = ComputeForwardParas()
+    Kernel = JacobiKernel(IterationCount, alpha, belta)
+
     print("Original Kernel:",Kernel)
 
     # Decomposition
